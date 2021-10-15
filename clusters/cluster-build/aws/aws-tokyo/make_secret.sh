ab4340f6c58a2d99ef44224b8874ef679070ad86
nickmerrett <nick.merrett@ibm.com>
2021-10-15 15:59:28 +1100

    swapped deployment_name for cluster_name

--- clusters/cluster-build/aws/aws-tokyo/make_secret.sh
+++ clusters/cluster-build/aws/aws-tokyo/make_secret.sh
@@ -21,8 +21,8 @@ if [[ -z ${PULL_SECRET} ]]; then
   exit 1
 fi
 
-if [[ -z ${DEPLOYMENT_NAME} ]]; then
-  echo "Please provide environment varaible for DEPLOYMENT_NAME, which is the name of the cluster, this should be the same in the values file."
+if [[ -z ${CLUSTER_NAME} ]]; then
+  echo "Please provide environment varaible for CLUSTER_NAME, which is the name of the cluster, this should be the same in the values file."
   exit 1
 fi
 
@@ -32,10 +32,10 @@ SEALED_SECRET_CONTROLLER_NAME=${SEALED_SECRET_CONTROLLER_NAME:-sealed-secrets}
 
 # Encrypt the secret using kubeseal and private key from the cluster
 echo "Creating Secrets"
-ENC_AWS_ID=$(echo -n ${AWS_ID} | kubeseal --raw --name=$DEPLOYMENT_NAME-aws-creds --namespace=$DEPLOYMENT_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
-ENC_AWS_KEY=$(echo -n ${AWS_KEY} | kubeseal --raw --name=$DEPLOYMENT_NAME-aws-creds --namespace=$DEPLOYMENT_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME  --from-file=/dev/stdin)
-ENC_PULL_SECRET=$(echo -n ${PULL_SECRET} | kubeseal --raw --name=$DEPLOYMENT_NAME-pull-secret --namespace=$DEPLOYMENT_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
-ENC_SSH_PRIV=$(cat ${SSH_PRIV_FILE} | kubeseal --raw --name=$DEPLOYMENT_NAME-ssh-private-key --namespace=$DEPLOYMENT_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_AWS_ID=$(echo -n ${AWS_ID} | kubeseal --raw --name=$CLUSTER_NAME-aws-creds --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_AWS_KEY=$(echo -n ${AWS_KEY} | kubeseal --raw --name=$CLUSTER_NAME-aws-creds --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME  --from-file=/dev/stdin)
+ENC_PULL_SECRET=$(echo -n ${PULL_SECRET} | kubeseal --raw --name=$CLUSTER_NAME-pull-secret --namespace=$CLUSTER_NAME --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
+ENC_SSH_PRIV=$(cat ${SSH_PRIV_FILE} | kubeseal --raw --name=$CLUSTER_NAME-ssh-private-key --namespace=$CLUSTER_NAME  --controller-namespace $SEALED_SECRET_NAMESPACE --controller-name $SEALED_SECRET_CONTROLLER_NAME --from-file=/dev/stdin)
 
 
 sed -i '' -e 's#.*aws_access_key_id.*$#    aws_access_key_id: '$ENC_AWS_ID'#g' values.yaml
